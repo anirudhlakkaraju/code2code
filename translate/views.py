5b2554892a9c8726b4c596c22c09ea9ae8d3b5d5
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-04-29 16:15:17 +0530

    Added home and result views

--- translate/views.py
+++ translate/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
+from .forms import InputForm
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', {})
+
+    if request.method=='POST':  
+        # Create a form instance and populate with data given by user
+        form = InputForm(request.POST)
+
+        # Checking validity: 
+        if form.is_valid(): 
+            return HttpResponseRedirect('result')
+
+    else: 
+        form = InputForm()
+
+    return render(request, 'home.html', {'form': form})
+
+def result(request):
+    return render(request, 'result.html')
