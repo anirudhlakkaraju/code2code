e0ac795a645e840223e014f0b318145a49276f5a
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-05 16:52:01 +0530

    Added translate URL

--- translate/urls.py
+++ translate/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('result', views.result, name='result')
+    path('translate', views.translate, name='translate')
 ]
