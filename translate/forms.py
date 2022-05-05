570fddb891cc71a47d534795016ba5a733ce17d8
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-05 18:29:34 +0530

    field name change

--- translate/forms.py
+++ translate/forms.py
@@ -6,9 +6,9 @@ class InputForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(InputForm, self).__init__(*args, **kwargs)
-        self.fields['target_code'].disabled = True
+        self.fields['translated_code'].disabled = True
 
     class Meta:
         model = Input
 
-        fields = ['source_language', 'source_code', 'target_language', 'target_code']
+        fields = ['source_language', 'source_code', 'target_language', 'translated_code']
