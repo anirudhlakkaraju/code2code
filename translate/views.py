62a333ffa42b32f3f5381cda171b73fabe104508
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-07 13:44:11 +0530

    Added translate view

--- translate/views.py
+++ translate/views.py
@@ -1,5 +1,6 @@
+from urllib import response
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponseRedirect
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 from rest_framework import viewsets 
 from rest_framework.decorators import api_view 
 from django.core import serializers 
@@ -20,42 +21,61 @@ from django.shortcuts import render, redirect
 from django.contrib import messages 
 
 
-class CodeTranslation(viewsets.ModelViewSet):
-    queryset = Input.objects.all()
-    serializer_class = InputSerializer
+# class CodeTranslation(viewsets.ModelViewSet):
+#     queryset = Input.objects.all()
+#     serializer_class = InputSerializer
+
+# def is_ajax(request):
+#     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
 def home(request):
 
-    if request.method=='POST':  
-        # Create a form instance and populate with data given by user
-        form = InputForm(request.POST or None)
-
-        # Checking validity: 
-        if form.is_valid(): 
+    # if request.method=='POST' :  
+    #     # Create a form instance and populate with data given by user
+    #     form = InputForm(request.POST)
+    #     # Checking validity: 
+    #     if form.is_valid(): 
             
-            # Extract user inputs
-            source_language = form.cleaned_data['source_language']
-            source_code = form.cleaned_data['source_code']
-            target_language = form.cleaned_data['target_language']
+    #         # Extract user inputs
+    #         source_language = form.cleaned_data['source_language']
+    #         source_code = form.cleaned_data['source_code']
+    #         target_language = form.cleaned_data['target_language']
 
-            # DO STUFF WITH DATA
-            # 1. Test with small model
-            # 2. Make it pretty
-            # 3. Ajax (reroute to same page)
-            # 4. Connect actual model
-            # 5. Create rest API
-                
-            # Prediction is done and we have target_code.txt
+    #         # PREDICT TARGET CODE
 
-            return HttpResponseRedirect('result')
+    #         translated_code = source_code
+    #         return json.dumps({"submitted":"True"})
 
-
-
+    
+    
     # Creates empty form on GET request
-    else: 
-        form = InputForm()
-
+    form = InputForm()
     return render(request, 'home.html', {'form': form})
 
-def result(request):
-    return render(request, 'result.html')
+
+def translate(request):
+    
+    # Receive the inputs as a POST request from AJAX call
+    if request.method=='POST':
+
+        # Load user inputs
+        response = json.loads(request.body)
+
+        # print(response)
+
+        # Extract user inputs
+        source_language = response['sourceLanguage']
+        source_code = response['sourceCode']
+        target_language = response['targetLanguage']
+
+        # PREDICT TARGET CODE
+        # ....
+
+        translated_code = source_code
+    
+        # Return translated code to AJAX call       
+        return JsonResponse({"Response":translated_code})
+
+    else:
+        # Empty Endpoint
+        return HttpResponse("Works!")
