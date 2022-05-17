7caf6d799fdfd09cc68f8e27e897b9462b7e33b8
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-17 16:29:27 +0530

    deleted extra comments

--- translate/views.py
+++ translate/views.py
@@ -20,41 +20,13 @@ import json
 import numpy as np 
 from sklearn import preprocessing 
 import pandas as pd 
+import time
 from django.shortcuts import render, redirect 
 from django.contrib import messages 
 
 
-
-# def process_python_output(prediction):
-#     lines = prediction.split("NEW_LINE")
-#     curr_indent = 0
-#     new_lines = []
-#     for line in lines:
-#         indent_count = line.count('INDENT')
-#         dedent_count = line.count('DEDENT')
-#         curr_indent += indent_count - dedent_count
-#         new_lines.append('\t'*curr_indent + line.replace('INDENT', '').replace('DEDENT', ''))
-#     return "\n".join(new_lines)
-
-
-# def predict(source_language, target_language, source_code):
-
-#     device = torch.device("cuda")
-#     model_type = "plbart"
-#     model, tokenizer = get_model(model_type, source_language, target_language)
-#     model.to(device)
-
-#     eval_batch_size = 1
-#     max_source_length, max_target_length = 400, 400
-
-#     eval_examples, eval_dataloader = get_eval_dataloader(source_code, eval_batch_size, 
-#                                                         max_source_length, max_target_length, tokenizer)
-
-#     pred = sample_generation_single(eval_examples, eval_dataloader, model, model_type, tokenizer, 
-#                             max_target_length, device)
-
-#     return process_python_output(pred[0])
-
+def index(request):
+    return HttpResponseRedirect('/home')
 
 def home(request):
     
@@ -81,13 +53,15 @@ def translate(request):
             return JsonResponse({"Response":source_code})
 
         # Predict target code
-        translated_code = predict(source_language, target_language, source_code)
+        # translated_code = predict(source_language, target_language, source_code)
 
         # Make translated code pretty
-        translated_code = process_outputs.pretty(translated_code, target_language)
+        # translated_code = process_outputs.pretty(translated_code, target_language)
+
+        # time.sleep(3)
 
         # Return translated code to AJAX call       
-        return JsonResponse({"Response":translated_code})
+        return JsonResponse({"Response":source_code})
 
     else:
         # Empty Endpoint
