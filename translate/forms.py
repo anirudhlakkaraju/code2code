88e417bc9f701cf247dc3b6aecd703efd486a1d5
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-23 18:11:26 +0530

    integrated ace widget

--- translate/forms.py
+++ translate/forms.py
@@ -1,14 +1,19 @@
 from django.forms import ModelForm
 from .models import Input
-
+from django_ace import AceWidget
 
 class InputForm(ModelForm):
 
-    # def __init__(self, *args, **kwargs):
-    #     super(InputForm, self).__init__(*args, **kwargs)
-    #     self.fields['translated_code'].disabled = True
-
     class Meta:
+
+        # Model from which forms' fields are from
         model = Input
 
+        # Adding ACE editor 
+        # widgets = {
+        #     'source_code': AceWidget(mode="python", theme="twilight"),
+        #     'translated_code': AceWidget(mode="python", theme="chrome"),
+        # }
+
+        # The fields to include from Input model
         fields = ['source_language', 'source_code', 'target_language', 'translated_code']
