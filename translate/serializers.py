ef898522201b959d5e329c65610279390d807ecd
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-05-24 17:15:45 +0530

    deleted serializers.py

--- translate/serializers.py
+++
@@ -1,8 +0,0 @@
-from re import I
-from rest_framework import serializers
-from .models import Input
-
-class InputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Input
-        fields = "__all__"
