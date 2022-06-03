edee36857f0b341b80856ebc61338f96d1548a33
anirudhlakkaraju <lakanirudh123@gmail.com>
2022-06-03 15:24:40 +0530

    added comments

--- translate/models.py
+++ translate/models.py
@@ -10,11 +10,15 @@ class Input(models.Model):
 
     source_language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, default='C++')
     source_code = models.TextField()
+
+    # In case test cases is implemented. UNUSED FOR NOW
     source_testcase = models.TextField()
     source_output = models.TextField()
     
     target_language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, default='Python')
     translated_code = models.TextField(blank=True)
+    
+    # In case test cases is implemented. UNUSED FOR NOW
     target_testcase = models.TextField()
     target_output = models.TextField()
 
